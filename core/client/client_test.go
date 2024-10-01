41b6f1f24ab362870daa702b0be55975fcfed424
NoobForAl <58856931+NoobforAl@users.noreply.github.com>
2024-10-01 18:50:13 +0330

    OPT-253 add: get data unit
    Signed-off-by: NoobForAl <58856931+NoobforAl@users.noreply.github.com>

--- core/client/client_test.go
+++ core/client/client_test.go
@@ -11,22 +11,40 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestClient(t *testing.T) {
+func TestMain(m *testing.M) {
 	go ncSdkMock.Run()
 
 	// wait for server to start
 	time.Sleep(1 * time.Second)
 
+	m.Run()
+}
+
+func TestClient(t *testing.T) {
 	mockClient := New("test", "test", "http://localhost:1234")
 
-	// Test GetDailyReads
-	dataResMock, err := mockClient.GetDailyReads("x_1234", schema.TimeRequest{Date: time.Now()})
-	assert.Nil(t, err)
+	t.Run("TestGetDailyReads", func(t *testing.T) {
+		dataResMock, err := mockClient.GetDailyReads("x_1234", schema.TimeRequest{Date: time.Now()})
+		assert.Nil(t, err)
 
-	// check it with sample mock data
-	var dataMock []*schema.MeterData
-	err = json.Unmarshal([]byte(ncMockSample.DailyReadsSample), &dataMock)
-	assert.Nil(t, err)
+		// check it with sample mock data
+		var dataMock []*schema.MeterData
+		err = json.Unmarshal([]byte(ncMockSample.DailyReadsSample), &dataMock)
+		assert.Nil(t, err)
 
-	assert.Equal(t, dataMock, dataResMock)
+		assert.Equal(t, dataMock, dataResMock)
+	})
+
+	t.Run("TestGetUnits", func(t *testing.T) {
+		unitsResMock, err := mockClient.GetUnits("x_1234")
+		assert.Nil(t, err)
+
+		// check it with sample mock data
+		var unitsMock []*schema.Unit
+		err = json.Unmarshal([]byte(ncMockSample.UnitsSample), &unitsMock)
+		assert.Nil(t, err)
+
+		assert.Equal(t, unitsMock, unitsResMock)
+	})
+
 }
